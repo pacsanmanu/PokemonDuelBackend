7c7b7bdf7d2181196d3b175de6db0f49e12c3d17
Manuel Pacheco <mpdevlp@gmail.com>
2024-02-25 20:56:59 +0100

    Configured CORS to test the app from a frontend

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 import { init } from './loaders/index.js';
 import config from './config.js';
 
 const app = express();
+
+const corsOptions = {
+	origin: 'http://localhost:3001'
+};
+
+app.use(cors(corsOptions));
 
 init(app, config);
 
