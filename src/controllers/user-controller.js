3c4b034ebc8691a1faa241779f56fda9374430dd
Manuel Pacheco <119961587+pacsanmanu@users.noreply.github.com>
2024-03-08 20:31:17 +0100

    Improved users services and controllers. CRUD completed for users

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,4 +1,4 @@
-import { getUsers, createUser, deleteUser, getUserByName } from '../services/mongodb/user-db-service.js';
+import { getUsers, createUser, deleteUser, getUserByName, updateUser } from '../services/mongodb/user-db-service.js';
 import { encryptPassword } from '../utils/encrypt.js';
 
 export async function getUsersController(req, res, next){
@@ -21,20 +21,61 @@ export async function getUserMe(req, res, next){
 	}
 }
 
-export async function createUserController(req, res, next){
-	try{
-		const body = req.body;
-		body.password = await encryptPassword(body.password);	
-		const users = await createUser(req.body);
-		return res.status(201).send(users);
-	} catch (error){
-		if(error.code === 11000){
-			error.status = 409;
-		}
-		if(error.message.includes('validation')){
-			error.status = 400;
-		}
-		next(error);
+export async function createUserController(req, res, next) {
+	try {
+			const { username, email, password } = req.body;
+			const usernameRegex = /^[a-zA-Z0-9]+$/;
+			const passwordRegex = /^[a-zA-Z0-9]+$/;
+			const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+			if (!emailRegex.test(email)) {
+					return res.status(400).send({ message: 'Invalid email format' });
+			}
+			if (!usernameRegex.test(username)) {
+					return res.status(400).send({ message: 'Username can only contain letters and numbers' });
+			}
+			if (!passwordRegex.test(password)) {
+					return res.status(400).send({ message: 'Password can only contain letters and numbers' });
+			}
+			req.body.password = await encryptPassword(password);  
+			const user = await createUser(req.body);
+			return res.status(201).send(user);
+	} catch (error) {
+			if (error.code === 11000) {
+					error.status = 409;
+			} else if (error.message.includes('validation')) {
+					error.status = 400;
+			}
+			next(error);
+	}
+}
+
+
+export async function updateUserController(req, res, next) {
+	try {
+			const { username, email, password } = req.body;
+			const userId = req.params.id;
+			const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+			const usernameRegex = /^[a-zA-Z0-9]+$/;
+			const passwordRegex = /^[a-zA-Z0-9]+$/;
+			if (email && !emailRegex.test(email)) {
+					return res.status(400).send({ message: 'Invalid email format' });
+			}
+			if (username && !usernameRegex.test(username)) {
+					return res.status(400).send({ message: 'Username can only contain letters and numbers' });
+			}
+			if (password && !passwordRegex.test(password)) {
+					return res.status(400).send({ message: 'Password can only contain letters and numbers' });
+			}
+			if (password) {
+					req.body.password = await encryptPassword(password);
+			}
+			const updatedUser = await updateUser(userId, req.body);
+			if (!updatedUser) {
+					return res.status(404).send({ message: 'User not found' });
+			}
+			res.status(200).send(updatedUser);
+	} catch (error) {
+			next(error);
 	}
 }
 
--- src/routes/user-router.js
+++ src/routes/user-router.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import {getUsersController, createUserController, deleteUserController ,getUserMe} from '../controllers/user-controller.js';
+import {getUsersController, createUserController, deleteUserController ,getUserMe, updateUserController} from '../controllers/user-controller.js';
 import { checkToken } from '../middlewares/auth-middleware.js';
 
 const router = Router();
+router.post('/', createUserController);
 router.get('/', checkToken, getUsersController);
 router.get('/me', checkToken, getUserMe);
-router.post('/', createUserController);
+router.patch('/:id', checkToken, updateUserController);
 router.delete('/:id', checkToken, deleteUserController);
 
 export default router;
