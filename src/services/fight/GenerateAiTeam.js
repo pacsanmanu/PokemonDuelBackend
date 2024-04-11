e7c8f777904f25874805887a657f25e24c36b78b
Manuel Pacheco <mpdevlp@gmail.com>
2024-04-11 12:52:16 +0200

    Improved AI team generation. Now based on user victories

--- src/services/fight/GenerateAiTeam.js
+++ src/services/fight/GenerateAiTeam.js
@@ -1,72 +1,74 @@
 import User from '../../models/user.js';
 import Pokemon from '../../models/pokemon.js';
-import { getPokemonsByNames } from '../mongodb/pokemon-db-service.js';
 import logger from '../../utils/logger.js';
 
-const getSimilarPokemonsByStats = async (avgStats, teamSize) => {
-  try {
-    const tolerance = 20;
-    const avgTotalStats = Object.values(avgStats).reduce((acc, val) => acc + val, 0);
-    logger.info(`Average Total Stats: ${avgTotalStats}`);
+function getGroupIndexByVictories(victories) {
+  if (victories <= 2) return [300, 315, 330];
+  if (victories <= 6) return [345, 360, 375, 390];
+  if (victories <= 10) return [405, 420, 435, 450];
+  if (victories <= 14) return [465, 480, 495, 510];
+  if (victories <= 17) return [525, 540, 555];
+  if (victories <= 21) return [570, 585, 600, 615];
+  if (victories <= 24) return [630, 645, 660];
+  if (victories <= 28) return [675, 690, 705, 720];
+  if (victories <= 33) return [735, 750, 765, 780, 795];
+  return [810, 825, 840, 855, 870, 885, 900, 1245];
+}
 
-    let matchStage;
-    if (avgTotalStats > 800) {
-      matchStage = {
-        $match: {
-          $expr: {
-            $gte: [
-              { $add: ['$stats.life', '$stats.attack', '$stats.defense', '$stats.specialAttack', '$stats.specialDefense', '$stats.speed'] },
-              800,
+const generateAiTeam = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      throw new Error('User not found.');
+    }
+
+    const statsGroups = getGroupIndexByVictories(user.victories);
+
+    const aggregationPipeline = [
+      {
+        $addFields: {
+          totalStats: {
+            $add: [
+              '$stats.life',
+              '$stats.attack',
+              '$stats.defense',
+              '$stats.specialAttack',
+              '$stats.specialDefense',
+              '$stats.speed',
             ],
           },
         },
-      };
-    } else {
-      matchStage = {
-        $match: {
-          $and: Object.keys(avgStats).map((key) => ({
-            [`stats.${key}`]: { $gte: avgStats[key] - tolerance, $lte: avgStats[key] + tolerance },
-          })),
+      },
+      {
+        $addFields: {
+          statsGroup: {
+            $subtract: [
+              '$totalStats',
+              { $mod: ['$totalStats', 15] },
+            ],
+          },
         },
-      };
-    }
+      },
+      {
+        $match: {
+          statsGroup: { $in: statsGroups },
+        },
+      },
+      {
+        $sample: { size: user.team.length },
+      },
+      {
+        $project: { _id: 0, name: 1 },
+      },
+    ];
 
-    const sampleStage = { $sample: { size: teamSize } };
-    const projectStage = { $project: { _id: 0, name: 1 } };
-
-    const aggregationPipeline = [matchStage, sampleStage, projectStage];
     const similarPokemons = await Pokemon.aggregate(aggregationPipeline);
 
     return similarPokemons.map((pokemon) => pokemon.name);
   } catch (error) {
-    logger.error('Error getting similar Pokemons by stats using aggregation', error);
+    logger.error('Error generating AI team', error);
     return [];
   }
 };
 
-export default async function generateAiTeam(userId) {
-  try {
-    const user = await User.findById(userId);
-    const userPokemons = await getPokemonsByNames(user.team);
-
-    const avgStats = userPokemons.reduce((acc, pokemon) => {
-      Object.keys(pokemon.stats).forEach((key) => {
-        if (!acc[key]) acc[key] = 0;
-        acc[key] += pokemon.stats[key];
-      });
-      return acc;
-    }, {});
-
-    Object.keys(avgStats).forEach((key) => {
-      avgStats[key] /= userPokemons.length;
-    });
-
-    const AiTeam = await getSimilarPokemonsByStats(avgStats, user.team.length);
-
-    logger.info(AiTeam);
-    return AiTeam;
-  } catch (error) {
-    logger.error('Error generating AI team', error);
-    return [];
-  }
-}
+export default generateAiTeam;
